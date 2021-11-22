cec0a9d662f07dd719eb93e1d8e555fbbc7a349e
Lucie <luciedumas24430@gmail.com>
2021-11-22 15:00:22 +0100

    correction de la page de conneXion

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
         </li>
         <!-- Button trigger modal -->
         <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-          Connection
+          Connexion
         </button>
       </ul>
     </header>
@@ -74,7 +74,7 @@
           <div class="modal-dialog">
             <div class="modal-content">
               <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Connection</h5>
+                <h5 class="modal-title" id="exampleModalLabel">Connexion</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               </div>
               <div class="modal-body">
