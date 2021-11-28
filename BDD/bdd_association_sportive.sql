4ac1cd0ef4f075cea62511404abe42c88a53f85f
Salomé <bonjour@salome-c.xyz>
2021-11-28 22:09:07 +0100

    un peu de ménage

--- BDD/bdd_association_sportive.sql
+++ BDD/bdd_association_sportive.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : sam. 27 nov. 2021 à 18:08
+-- Généré le : sam. 27 nov. 2021 à 20:41
 -- Version du serveur :  5.7.31
 -- Version de PHP : 7.3.21
 
@@ -58,31 +58,6 @@ INSERT INTO `categorie` (`id`, `nom_categorie`, `tranche_age`) VALUES
 -- --------------------------------------------------------
 
 --
--- Structure de la table `entrainement`
---
-
-DROP TABLE IF EXISTS `entrainement`;
-CREATE TABLE IF NOT EXISTS `entrainement` (
-  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `id_equipe_1` int(11) UNSIGNED NOT NULL,
-  `id_equipe_2` int(11) UNSIGNED NOT NULL,
-  `lieu` varchar(50) NOT NULL COMMENT 'stade - ville',
-  `date_heure` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `id_equipe_2` (`id_equipe_2`),
-  KEY `id_equipe_1` (`id_equipe_1`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `entrainement`
---
-
-INSERT INTO `entrainement` (`id`, `id_equipe_1`, `id_equipe_2`, `lieu`, `date_heure`) VALUES
-(1, 1, 1, '50 rue de la poutre - LAVILLE', '2021-11-30 22:43:49');
-
--- --------------------------------------------------------
-
---
 -- Structure de la table `entraineur`
 --
 
@@ -91,7 +66,7 @@ CREATE TABLE IF NOT EXISTS `entraineur` (
   `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `nom_ent` varchar(25) NOT NULL,
   `prenom_ent` varchar(25) NOT NULL,
-  `id_equipe` int(11) UNSIGNED NOT NULL,
+  `id_equipe` int(11) UNSIGNED DEFAULT NULL,
   `identifiant_ent` varchar(25) NOT NULL COMMENT 'Pour le login uniquement',
   `mdp_ent` varchar(25) NOT NULL,
   PRIMARY KEY (`id`),
@@ -111,8 +86,7 @@ INSERT INTO `entraineur` (`id`, `nom_ent`, `prenom_ent`, `id_equipe`, `identifia
 (7, 'CYRIAL-LIGNIQUE', 'Sissy', 9, 'SissyCL', 'C_kr0k-n'),
 (8, 'PÉKINOIS', 'Lou', 10, 'LouP', 'S1T-P/PL'),
 (9, 'CRISE', 'Jésus', 11, 'JesusC', 'AnCpQiPmf@'),
-(10, 'TRISTE', 'Jésus', 12, 'JesusT', '@nCpQcMiPMF-'),
-(11, 'PERRUCHE', 'Lucy', 1, 'LucyP', 'Ess@f4i_B1MB-MB0Om');
+(10, 'TRISTE', 'Jésus', 12, 'JesusT', '@nCpQcMiPMF-');
 
 -- --------------------------------------------------------
 
@@ -123,11 +97,11 @@ INSERT INTO `entraineur` (`id`, `nom_ent`, `prenom_ent`, `id_equipe`, `identifia
 DROP TABLE IF EXISTS `equipe`;
 CREATE TABLE IF NOT EXISTS `equipe` (
   `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `nom_equipe` varchar(25) CHARACTER SET utf8 NOT NULL,
+  `nom_equipe` varchar(25) NOT NULL,
   `id_categorie` int(11) UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
   KEY `id_categorie` (`id_categorie`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `equipe`
@@ -157,7 +131,7 @@ CREATE TABLE IF NOT EXISTS `joueur` (
   `nom_joueur` varchar(25) NOT NULL,
   `prenom_joueur` varchar(25) NOT NULL,
   `age_joueur` tinyint(2) NOT NULL,
-  `id_equipe` int(11) UNSIGNED NOT NULL,
+  `id_equipe` int(11) UNSIGNED DEFAULT NULL,
   `identifiant_joueur` varchar(25) NOT NULL COMMENT 'Pour le login uniquement',
   `mdp_joueur` varchar(25) NOT NULL,
   PRIMARY KEY (`id`),
@@ -169,41 +143,18 @@ CREATE TABLE IF NOT EXISTS `joueur` (
 --
 
 INSERT INTO `joueur` (`id`, `nom_joueur`, `prenom_joueur`, `age_joueur`, `id_equipe`, `identifiant_joueur`, `mdp_joueur`) VALUES
-(1, 'MENVUSSA', 'Gérard', 12, 1, 'GerardM', 'jivouari1_4Y4Y'),
-(2, 'DEUSSAVOUA', 'Tom', 15, 2, 'TomD', 'D0ubleCh1zz'),
-(3, 'PEPAY', 'Gilles', 15, 1, 'GillesP', 'D_Z4rouA'),
-(4, 'CLAQUAIS', 'Robernard', 40, 2, 'RobC', 'P3digrEe_OOF'),
-(5, 'CATAPOUTTE', 'Jessicaren', 30, 5, 'JessC', '3ye_L1nAH'),
-(6, 'ETCHEPOUËT', 'Philippe', 25, 5, 'PhilE', '0h/C_kxns@'),
-(7, 'DEGNIART', 'Bronis', 49, 6, 'BronisD', 'H0m_Kxnstrui/AH'),
-(8, 'IFADIA	', 'Fadi', 24, 6, 'FadiI', 'Ch3rN0B-L'),
-(9, 'Monte-Hanié', 'Gilbert', 27, 7, 'GilbertM', 'F3st1vAL-Kn'),
-(10, 'TRAILLETTE', 'Amy', 37, 7, 'AmyT', 'R4tat@'),
-(11, 'LAMPALED', 'Stracy', 36, 8, 'StracyL', 'J4NoSkuRre'),
-(12, 'SAINTI', 'Ignace', 46, 8, 'IgnaceS', 'F3At_l-O2');
-
--- --------------------------------------------------------
-
---
--- Structure de la table `match`
---
-
-DROP TABLE IF EXISTS `match`;
-CREATE TABLE IF NOT EXISTS `match` (
-  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `id_equipe` int(11) UNSIGNED NOT NULL,
-  `lieu` varchar(50) NOT NULL COMMENT 'stade - ville',
-  `date_heure` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `id_equipe_bis` (`id_equipe`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `match`
---
-
-INSERT INTO `match` (`id`, `id_equipe`, `lieu`, `date_heure`) VALUES
-(1, 1, '50 rue de la poutre - LAVILLE', '2021-11-30 21:23:11');
+(1, 'MENVUSSA', 'Gérard', 5, 1, 'GerardM', 'jivouari1_4Y4Y'),
+(2, 'DEUSSAVOUA', 'Tom', 8, 2, 'TomD', 'D0ubleCh1zz'),
+(3, 'PEPAY', 'Gilles', 7, 1, 'GillesP', 'D_Z4rouA'),
+(4, 'CLAQUAIS', 'Robernard', 9, 2, 'RobC', 'P3digrEe_OOF'),
+(5, 'CATAPOUTTE', 'Jessicaren', 10, 5, 'JessC', '3ye_L1nAH'),
+(6, 'ETCHEPOUËT', 'Philippe', 10, 5, 'PhilE', '0h/C_kxns@'),
+(7, 'DEGNIART', 'Bronis', 12, 6, 'BronisD', 'H0m_Kxnstrui/AH'),
+(8, 'IFADIA	', 'Fadi', 12, 6, 'FadiI', 'Ch3rN0B-L'),
+(9, 'Monte-Hanié', 'Gilbert', 15, 7, 'GilbertM', 'F3st1vAL-Kn'),
+(10, 'TRAILLETTE', 'Amy', 14, 7, 'AmyT', 'R4tat@'),
+(11, 'LAMPALED', 'Stracy', 17, 8, 'StracyL', 'J4NoSkuRre'),
+(12, 'SAINTI', 'Ignace', 17, 8, 'IgnaceS', 'F3At_l-O2');
 
 -- --------------------------------------------------------
 
@@ -233,13 +184,6 @@ INSERT INTO `president` (`id`, `nom_pres`, `prenom_pres`, `identifiant_pres`, `m
 --
 
 --
--- Contraintes pour la table `entrainement`
---
-ALTER TABLE `entrainement`
-  ADD CONSTRAINT `entrainement_ibfk_1` FOREIGN KEY (`id_equipe_1`) REFERENCES `equipe` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `entrainement_ibfk_2` FOREIGN KEY (`id_equipe_2`) REFERENCES `equipe` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
 -- Contraintes pour la table `entraineur`
 --
 ALTER TABLE `entraineur`
@@ -256,12 +200,6 @@ ALTER TABLE `equipe`
 --
 ALTER TABLE `joueur`
   ADD CONSTRAINT `joueur_ibfk_1` FOREIGN KEY (`id_equipe`) REFERENCES `equipe` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Contraintes pour la table `match`
---
-ALTER TABLE `match`
-  ADD CONSTRAINT `match_ibfk_1` FOREIGN KEY (`id_equipe`) REFERENCES `equipe` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
